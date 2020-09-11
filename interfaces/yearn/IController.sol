4e9cd166dfdd8264e55cadd4ba03fa275f843359
Bryant <fubuloubu@gmail.com>
2020-09-11 16:28:01 -0400

    Vault[USDC,yCRV]: 2020-07-26
    yUSDC - https://etherscan.io/address/0x597aD1e0c13Bfe8025993D9e79C69E1c0233522e#code
    yyCRV - https://etherscan.io/address/0x5dbcF33D8c2E976c6b560249878e6F1491Bca25c#code

---
+++ interfaces/yearn/IController.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.16;
+
+interface IController {
+    function withdraw(address, uint) external;
+    function balanceOf(address) external view returns (uint);
+    function earn(address, uint) external;
+}
