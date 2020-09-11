1d37c21028a8e302f4189973545256a22a70511d
Bryant <fubuloubu@gmail.com>
2020-09-11 17:01:31 -0400

    Vault[WETH]: 2020-09-01
    yWETH - https://etherscan.io/address/0xe1237aA7f535b0CC33Fd973D66cBf830354D16c7#code

---
+++ interfaces/weth/WETH.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.16;
+
+interface WETH {
+    function deposit() external payable;
+    function withdraw(uint wad) external;
+    event Deposit(address indexed dst, uint wad);
+    event Withdrawal(address indexed src, uint wad);
+}
