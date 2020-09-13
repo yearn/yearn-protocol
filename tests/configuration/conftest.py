de0a1107dc36fbd97c349bf0476250fc6bebe3ae
Bryant <fubuloubu@gmail.com>
2020-09-13 11:58:02 -0400

    test: Add configuration checks for Controller/Vaults/Strategies

---
+++ tests/configuration/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+@pytest.fixture
+def rewards(a):
+    yield a[2]
+
+
+@pytest.fixture
+def gov(a):
+    yield a[3]
+
+
+@pytest.fixture
+def token(a, Token):
+    # Must be ERC20
+    yield a[0].deploy(Token)
+
+
+@pytest.fixture
+def controller(a):
+    yield a[4]
