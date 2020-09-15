c386f1ade1de539cd3ef286915469ec8d894a9b2
Luciano Bertenasco <lbertenasco@gmail.com>
2020-09-15 16:23:06 -0300

    use solidity 0.5.17

--- contracts/registries/YRegistry.sol
+++ contracts/registries/YRegistry.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.8;
+pragma solidity ^0.5.17;
 
 import "@openzeppelinV3/contracts/math/SafeMath.sol";
 import "@openzeppelinV3/contracts/utils/Address.sol";
