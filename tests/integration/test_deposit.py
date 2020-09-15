49c79346116852872507f55be792176fb55939c3
Bryant <fubuloubu@gmail.com>
2020-09-14 20:56:18 -0400

    test: Update deposit test to test more

--- tests/integration/test_deposit.py
+++ tests/integration/test_deposit.py
@@ -1,5 +1,13 @@
+import brownie
+
+
 def test_deposit_withdraw_shares(minnow, token, vault):
     balance = token.balanceOf(minnow)
+    assert vault.balanceOf(minnow) == 0
+
+    # Don't forget the approval
+    with brownie.reverts():
+        vault.deposit(balance // 3, {"from": minnow})
     token.approve(vault, balance, {"from": minnow})
 
     # Deposit 1/3 of entire balance
@@ -9,6 +17,7 @@ def test_deposit_withdraw_shares(minnow, token, vault):
         vault.balanceOf(minnow)
         == (balance // 3) * vault.totalSupply() // vault.balance()
     )
+
     # Deposit entire balance
     vault.depositAll({"from": minnow})  # 2/3
     # Shares show appropiate balance
@@ -16,11 +25,15 @@ def test_deposit_withdraw_shares(minnow, token, vault):
 
     # Withdraw 1/3 of entire balance
     vault.withdraw(vault.balanceOf(minnow) // 3, {"from": minnow})  # 1/3
-    # Shares show appropiate balance
+    # Shares show appropiate balance (Might be a little off)
     assert (
-        vault.balanceOf(minnow)
-        == (balance // 3) * vault.totalSupply() // vault.balance()
+        abs(
+            vault.balanceOf(minnow)
+            - 2 * (balance // 3) * vault.totalSupply() // vault.balance()
+        )
+        < 10  # This is fine, less than 1e-17 difference
     )
+
     # Withdraw entire balance
     vault.withdrawAll({"from": minnow})  # 2/3
     # Shares show appropiate balance
