a33a7e94a1d0d2f45a516c48c1e6bdb0591c921a
banteg <banteeg@gmail.com>
2020-09-19 21:47:52 +0700

    feat: Add BTC OSM proxy (#6)
    * add btc osm proxy
    
    * add ugly formatting for btc oracle, coerced by bot
    
    * change safemath to import in btc oracle
    
    * add tests for btc oracle
    
    * add foresight test
    
    * remove unneeded import

---
+++ tests/configuration/test_oracles.py
@@ -0,0 +1,45 @@
+import pytest
+
+
+@pytest.fixture
+def oracle(a, OSMedianizer):
+    yield a[0].deploy(OSMedianizer)
+
+
+def test_hardcoded_config(a, oracle):
+    assert oracle.OSM() == '0xf185d0682d50819263941e5f4EacC763CC5C6C42'
+    assert oracle.MEDIANIZER() == '0x9B8Eb8b3d6e2e0Db36F41455185FEF7049a35CaE'
+
+
+def test_governance(a, gov, oracle):
+    assert oracle.governance() == a[0]
+    oracle.setGovernance(gov)
+    assert oracle.governance() == gov
+
+
+def test_whitelist(gov, oracle):
+    assert not oracle.authorized(gov)
+    oracle.setAuthorized(gov)
+    assert oracle.authorized(gov)
+    oracle.revokeAuthorized(gov)
+    assert not oracle.authorized(gov)
+
+
+@pytest.mark.parametrize('func', ['read', 'foresight'])
+def test_read(a, oracle, func):
+    price, osm = getattr(oracle, func)()
+    assert price > 0
+    assert not osm
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('func', ['read', 'foresight'])
+def test_read_bud(a, interface, OSMedianizer, func):
+    oracle = OSMedianizer.at('0x82c93333e4E295AA17a05B15092159597e823e8a')
+    osm = interface.OracleSecurityModule(oracle.OSM())
+    assert osm.bud(oracle), 'kiss first'
+    reader = a[0]  # TODO: someone authorized
+    assert oracle.authorized(reader)
+    price, osm = getattr(oracle, func)({'from': reader})
+    assert price > 0
+    assert osm
