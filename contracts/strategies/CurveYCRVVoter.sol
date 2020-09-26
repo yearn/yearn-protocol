3e52bce00ac3376973a8ed369adc9aa67a549d11
banteg <banteeg@gmail.com>
2020-09-26 22:23:32 +0700

    refactor: delete redundant comments

--- contracts/strategies/CurveYCRVVoter.sol
+++ contracts/strategies/CurveYCRVVoter.sol
@@ -14,20 +14,6 @@ import "../../interfaces/curve/VoteEscrow.sol";
 import "../../interfaces/uniswap/Uni.sol";
 import "../../interfaces/yearn/Token.sol";
 
-/*
-
- A strategy must implement the following calls;
- 
- - deposit()
- - withdraw(address) must exclude any tokens used in the yield - Controller role - withdraw should return to Controller
- - withdraw(uint) - Controller | Vault role - withdraw should always return to vault
- - withdrawAll() - Controller | Vault role - withdraw should always return to vault
- - balanceOf()
- 
- Where possible, strategies must remain as immutable as possible, instead of updating variables, we update the contract by linking it in the controller
- 
-*/
-
 contract CurveYCRVVoter {
     using SafeERC20 for IERC20;
     using Address for address;
--- contracts/strategies/StrategyCurveYBUSDVoterProxy.sol
+++ contracts/strategies/StrategyCurveYBUSDVoterProxy.sol
@@ -15,20 +15,6 @@ import "../../interfaces/curve/Curve.sol";
 import "../../interfaces/yearn/Token.sol";
 import "../../interfaces/yearn/VoterProxy.sol";
 
-/*
-
- A strategy must implement the following calls;
- 
- - deposit()
- - withdraw(address) must exclude any tokens used in the yield - Controller role - withdraw should return to Controller
- - withdraw(uint) - Controller | Vault role - withdraw should always return to vault
- - withdrawAll() - Controller | Vault role - withdraw should always return to vault
- - balanceOf()
- 
- Where possible, strategies must remain as immutable as possible, instead of updating variables, we update the contract by linking it in the controller
- 
-*/
-
 contract StrategyCurveYBUSDVoterProxy {
     using SafeERC20 for IERC20;
     using Address for address;
