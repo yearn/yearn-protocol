42e262d0f1107efecdb939ee673538ee9f86a923
Bryant Eisenbach <fubuloubu@gmail.com>
2020-09-26 23:21:20 -0400

    test: Added TestStrategy for testing Strategy API functionality

---
+++ contracts/test/TestStrategy.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.6.12;
+
+import { BaseStrategy } from "../BaseStrategy.sol";
+
+/*
+ * This Strategy serves as both a mock Strategy for testing, and an example
+ * for integrators on how to use BaseStrategy
+ */
+
+contract TestStrategy is BaseStrategy {
+    constructor(address _vault, address _governance)
+        BaseStrategy(_vault, _governance)
+        public
+    { }
+
+    // When exiting the position, wait this many times to give everything back
+    uint countdownTimer = 3;
+
+    function expectedReturn()
+        public
+        view
+        override
+        returns (uint256 er)
+    {
+        (,,,,,, er) = vault.strategies(address(this));
+    }
+
+    function prepareReturn()
+        internal
+        override
+    {
+        // During testing, send this contract some tokens to simulate "Rewards"
+    }
+
+    function adjustPosition()
+        internal
+        override
+    {
+        // Whatever we have, consider it "invested" now
+        reserve = want.balanceOf(address(this));
+    }
+
+    function exitPosition()
+        internal
+        override
+    {
+        // Dump 25% each time this is called, the first 3 times
+        if (countdownTimer > 0) {
+            reserve -= want.balanceOf(address(this)).div(4);
+            countdownTimer -= 1;
+        } else {
+            reserve = 0;
+        }
+    }
+
+    function prepareMigration(address _newStrategy)
+        internal
+        override
+    {
+        want.transfer(_newStrategy, want.balanceOf(address(this)));
+    }
+}
