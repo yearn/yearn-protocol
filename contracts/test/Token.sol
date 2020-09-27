1fabe9d092bbeec0b8ce9312fae68fafcc2c49ac
Bryant Eisenbach <fubuloubu@gmail.com>
2020-09-26 23:01:37 -0400

    refactor: Upgrade the repo for no more fork testing! and Solidity 0.6.12

--- contracts/test/Token.sol
+++ contracts/test/Token.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.17;
+pragma solidity ^0.6.12;
 
-import "@openzeppelinV2/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelinV2/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelinV3/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20, ERC20Detailed {
-    constructor() public ERC20Detailed("yearn.finance test token", "TEST", 18) {
+contract Token is ERC20 {
+    constructor() public ERC20("yearn.finance test token", "TEST") {
         _mint(msg.sender, 30000 * 10**18);
     }
 }
