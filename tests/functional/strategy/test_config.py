bd2d57988cc3e8c854a7083c41aeaeefc943c24a
Bryant <fubuloubu@gmail.com>
2020-09-29 12:39:19 -0400

    test: Add tests for BaseStrategy

---
+++ tests/functional/strategy/test_config.py
@@ -0,0 +1,76 @@
+import brownie
+
+
+def test_strategy_deployment(strategist, gov, vault, TestStrategy):
+    strategy = strategist.deploy(TestStrategy, vault, gov)
+    # Addresses
+    assert strategy.governance() == gov
+    assert strategy.strategist() == strategist
+    assert strategy.keeper() == strategist
+    assert strategy.want() == vault.token()
+    assert strategy.reserve() == 0
+    assert strategy.performanceFee() == 500
+    assert not strategy.emergencyExit()
+
+
+def test_vault_setPerformanceFee(strategy, gov, strategist, rando):
+    # Only governance can set this param
+    with brownie.reverts():
+        strategy.setPerformanceFee(1000, {"from": rando})
+    with brownie.reverts():
+        strategy.setPerformanceFee(1000, {"from": strategist})
+    assert strategy.performanceFee() != 1000
+
+    strategy.setPerformanceFee(1000, {"from": gov})
+    assert strategy.performanceFee() == 1000
+
+
+def test_vault_setStrategist(strategy, gov, strategist, rando):
+    # Only governance or strategist can set this param
+    with brownie.reverts():
+        strategy.setStrategist(rando, {"from": rando})
+    assert strategy.strategist() != rando
+
+    strategy.setStrategist(rando, {"from": gov})
+    assert strategy.strategist() == rando
+
+    strategy.setStrategist(strategist, {"from": rando})
+    assert strategy.strategist() == strategist
+
+
+def test_strategy_setEmergencyExit(strategy, gov, strategist, rando, chain):
+    # Only governance or strategist can set this param
+    with brownie.reverts():
+        strategy.setEmergencyExit({"from": rando})
+    assert not strategy.emergencyExit()
+
+    strategy.setEmergencyExit({"from": gov})
+    assert strategy.emergencyExit()
+
+    # Can only set this once
+    chain.undo()
+
+    strategy.setEmergencyExit({"from": strategist})
+    assert strategy.emergencyExit()
+
+
+def test_strategy_setGovernance(strategy, gov, rando):
+    newGov = rando
+    # No one can set governance but governance
+    with brownie.reverts():
+        strategy.setGovernance(newGov, {"from": newGov})
+    # Governance doesn't change until it's accepted
+    strategy.setGovernance(newGov, {"from": gov})
+    assert strategy.governance() == gov
+    # Only new governance can accept a change of governance
+    with brownie.reverts():
+        strategy.acceptGovernance({"from": gov})
+    # Governance doesn't change until it's accepted
+    strategy.acceptGovernance({"from": newGov})
+    assert strategy.governance() == newGov
+    # No one can set governance but governance
+    with brownie.reverts():
+        strategy.setGovernance(newGov, {"from": gov})
+    # Only new governance can accept a change of governance
+    with brownie.reverts():
+        strategy.acceptGovernance({"from": gov})
