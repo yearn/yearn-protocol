94da5ec5d8b33520e795c7c5ec514586aec364c3
Bryant <fubuloubu@gmail.com>
2020-09-29 15:44:16 -0400

    test: Refactor ERC20 tests for shares

--- tests/functional/vault/test_share_erc20.py
+++ tests/functional/vault/test_share_erc20.py
@@ -4,11 +4,12 @@ import brownie
 
 @pytest.fixture
 def vault(gov, token, Vault):
+    # NOTE: Overriding the one in conftest because it has values already
     yield gov.deploy(Vault, token, gov, gov)
 
 
-def test_transfer(accounts, token, vault):
-    a, b, c = accounts[0:3]
+def test_transfer(accounts, token, vault, fn_isolation):
+    a, b = accounts[0:2]
     token.approve(vault, token.balanceOf(a), {"from": a})
     vault.deposit(token.balanceOf(a), {"from": a})
 
@@ -20,9 +21,33 @@ def test_transfer(accounts, token, vault):
     assert vault.balanceOf(a) == 0
     assert vault.balanceOf(b) == token.balanceOf(vault)
 
-    vault.approve(c, vault.balanceOf(b), {"from": b})
+
+def test_transferFrom(accounts, token, vault, fn_isolation):
+    a, b, c = accounts[0:3]
+    token.approve(vault, token.balanceOf(a), {"from": a})
+    vault.deposit(token.balanceOf(a), {"from": a})
+
+    # Unapproved can't send
     with brownie.reverts():
-        vault.transferFrom(b, a, vault.balanceOf(b), {"from": a})
+        vault.transferFrom(b, a, vault.balanceOf(b) // 2, {"from": c})
+
+    vault.approve(c, vault.balanceOf(b) // 2, {"from": b})
+    assert vault.allowance(b, c) == vault.balanceOf(b) // 2
+
+    # Can't send more than what is approved
+    with brownie.reverts():
+        vault.transferFrom(b, a, vault.balanceOf(b), {"from": c})
+
+    assert vault.balanceOf(a) == token.balanceOf(vault)
+    assert vault.balanceOf(b) == 0
+
+    vault.transferFrom(b, a, vault.balanceOf(b) // 2, {"from": c})
+
+    assert vault.balanceOf(a) == token.balanceOf(vault) // 2
+    assert vault.balanceOf(b) == token.balanceOf(vault) // 2
+
+    # If approval is unlimited, little bit of a gas savings
+    vault.approve(c, 2 ** 256 - 1, {"from": b})
     vault.transferFrom(b, a, vault.balanceOf(b), {"from": c})
 
     assert vault.balanceOf(a) == token.balanceOf(vault)
