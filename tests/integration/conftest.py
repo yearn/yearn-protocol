70726ab0a9113ae9fd123f8985381becfd37de7e
Bryant <fubuloubu@gmail.com>
2020-09-29 19:22:25 -0400

    refactor: Gave the conftest some funny things

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.fixture
 def andre(accounts):
-    # Has all the tokens
+    # Andre, giver of tokens, and maker of yield 
     yield accounts[0]
 
 
@@ -14,6 +14,7 @@ def token(andre, Token):
 
 @pytest.fixture
 def gov(accounts):
+    # yearn multis... I mean YFI governance. I swear!
     yield accounts[1]
 
 
@@ -24,21 +25,26 @@ def rewards(gov):
 
 @pytest.fixture
 def guardian(accounts):
+    # YFI Whale, probably
     yield accounts[2]
 
 
 @pytest.fixture
 def vault(gov, rewards, guardian, token, Vault):
-    yield guardian.deploy(Vault, token, gov, rewards)
+    vault = guardian.deploy(Vault, token, gov, rewards)
+    vault.setDebtLimit(token.totalSupply(), {"from": gov})  # Adjust from 0.1% to 100%
+    yield vault
 
 
 @pytest.fixture
 def strategist(accounts):
+    # You! Our new Strategist!
     yield accounts[3]
 
 
 @pytest.fixture
 def keeper(accounts):
+    # This is our trusty bot!
     yield accounts[4]
 
 
@@ -50,38 +56,62 @@ def strategy(gov, strategist, keeper, vault, TestStrategy):
 
 
 @pytest.fixture
-def pleb(accounts):
-    # Has no tokens
+def nocoiner(accounts):
+    # Has no tokens (DeFi is a ponzi scheme!)
     yield accounts[5]
 
 
 @pytest.fixture
-def minnow(accounts, andre, token):
+def pleb(accounts, andre, token, vault):
+    # Small fish in a big pond
     a = accounts[6]
-    # Has 0.01% of tokens
-    token.transfer(a, token.totalSupply() // 10000, {"from": andre})
+    # Has 0.01% of tokens (heard about this new DeFi thing!)
+    bal = token.totalSupply() // 10000
+    token.transfer(a, bal, {"from": andre})
+    # Unlimited Approvals
+    token.approve(vault, 2 ** 256 - 1, {"from": a})
+    # Deposit half their stack
+    vault.deposit(bal // 2, {"from": a})
     yield a
 
 
 @pytest.fixture
-def dolphin(accounts, andre, token):
+def chad(accounts, andre, token, vault):
+    # Just here to have fun!
     a = accounts[7]
-    # Has 0.1% of tokens
-    token.transfer(a, token.totalSupply() // 1000, {"from": andre})
+    # Has 0.1% of tokens (somehow makes money trying every new thing)
+    bal = token.totalSupply() // 1000
+    token.transfer(a, bal, {"from": andre})
+    # Unlimited Approvals
+    token.approve(vault, 2 ** 256 - 1, {"from": a})
+    # Deposit half their stack
+    vault.deposit(bal // 2, {"from": a})
     yield a
 
 
 @pytest.fixture
-def shark(accounts, andre, token):
+def greyhat(accounts, andre, token, vault):
+    # Chaotic evil, will eat you alive
     a = accounts[8]
-    # Has 1% of tokens
-    token.transfer(a, token.totalSupply() // 100, {"from": andre})
+    # Has 1% of tokens (earned them the *hard way*)
+    bal = token.totalSupply() // 100
+    token.transfer(a, bal, {"from": andre})
+    # Unlimited Approvals
+    token.approve(vault, 2 ** 256 - 1, {"from": a})
+    # Deposit half their stack
+    vault.deposit(bal // 2, {"from": a})
     yield a
 
 
 @pytest.fixture
-def whale(accounts, andre, token):
+def whale(accounts, andre, token, vault):
+    # Totally in it for the tech
     a = accounts[9]
-    # Has 10% of tokens
-    token.transfer(a, token.totalSupply() // 10, {"from": andre})
+    # Has 10% of tokens (was in the ICO)
+    bal = token.totalSupply() // 10
+    token.transfer(a, bal, {"from": andre})
+    # Unlimited Approvals
+    token.approve(vault, 2 ** 256 - 1, {"from": a})
+    # Deposit half their stack
+    vault.deposit(bal // 2, {"from": a})
     yield a
