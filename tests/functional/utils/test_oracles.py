0ced2334c8c193700b5d835d41c91ff1940094aa
milkyklim <10698619+milkyklim@users.noreply.github.com>
2020-10-07 08:37:21 +0200

    style: format python files

--- tests/functional/utils/test_oracles.py
+++ tests/functional/utils/test_oracles.py
@@ -7,8 +7,8 @@ def oracle(a, BTCOSMedianizer):
 
 
 def test_hardcoded_config(a, oracle):
-    assert oracle.OSM() == '0xf185d0682d50819263941e5f4EacC763CC5C6C42'
-    assert oracle.MEDIANIZER() == '0x9B8Eb8b3d6e2e0Db36F41455185FEF7049a35CaE'
+    assert oracle.OSM() == "0xf185d0682d50819263941e5f4EacC763CC5C6C42"
+    assert oracle.MEDIANIZER() == "0x9B8Eb8b3d6e2e0Db36F41455185FEF7049a35CaE"
 
 
 def test_governance(a, gov, oracle):
@@ -25,7 +25,7 @@ def test_whitelist(gov, oracle):
     assert not oracle.authorized(gov)
 
 
-@pytest.mark.parametrize('func', ['read', 'foresight'])
+@pytest.mark.parametrize("func", ["read", "foresight"])
 def test_read(a, oracle, func):
     price, osm = getattr(oracle, func)()
     assert price > 0
@@ -33,13 +33,13 @@ def test_read(a, oracle, func):
 
 
 @pytest.mark.xfail
-@pytest.mark.parametrize('func', ['read', 'foresight'])
+@pytest.mark.parametrize("func", ["read", "foresight"])
 def test_read_bud(a, interface, OSMedianizer, func):
-    oracle = OSMedianizer.at('0x82c93333e4E295AA17a05B15092159597e823e8a')
+    oracle = OSMedianizer.at("0x82c93333e4E295AA17a05B15092159597e823e8a")
     osm = interface.OracleSecurityModule(oracle.OSM())
-    assert osm.bud(oracle), 'kiss first'
+    assert osm.bud(oracle), "kiss first"
     reader = a[0]  # TODO: someone authorized
     assert oracle.authorized(reader)
-    price, osm = getattr(oracle, func)({'from': reader})
+    price, osm = getattr(oracle, func)({"from": reader})
     assert price > 0
     assert osm
