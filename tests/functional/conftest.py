84e506f08e8b130e418aa3756deb0798db8e010c
banteg <banteeg@gmail.com>
2020-10-07 02:42:05 +0200

    feat: add black config

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -24,14 +24,14 @@ def controller(a):
 
 @pytest.fixture
 def andre(accounts):
-    return accounts.at('0x2D407dDb06311396fE14D4b49da5F0471447d45C', force=True)
+    return accounts.at("0x2D407dDb06311396fE14D4b49da5F0471447d45C", force=True)
 
 
 @pytest.fixture
 def ychad(accounts):
-    return accounts.at('0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52', force=True)
+    return accounts.at("0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52", force=True)
 
 
 @pytest.fixture
 def binance(accounts):
-    return accounts.at('0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE', force=True)
+    return accounts.at("0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE", force=True)
