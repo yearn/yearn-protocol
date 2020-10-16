15afddb35ab296428eda6bcb70970836073030a0
dmolina79 <dmolina79@users.noreply.github.com>
2020-10-15 19:13:05 -0600

    feat: add sample CRV and UNI strats for CREAM

---
+++ tests/functional/strategies/test_strategy_crv_cream.py
@@ -0,0 +1,50 @@
+import pytest
+import brownie
+
+from brownie import Wei, convert, accounts
+
+from brownie import StrategyCreamCRV, Controller, yVault
+
+MAX_LIMIT = 2 ** 256 - 1
+
+# CRV
+TOKEN_CONTRACT = "0xD533a949740bb3306d119CC777fa900bA034cd52"
+
+STRAT_NAME = "StrategyCreamCRV"
+
+
+@pytest.fixture
+def token(Contract):
+    yield Contract.from_explorer(TOKEN_CONTRACT)
+
+
+@pytest.fixture
+def strategy(gov, controller):
+    strategy = gov.deploy(StrategyCreamCRV, controller)
+    return strategy
+
+
+@pytest.fixture
+def vault(gov, controller, strategy, token):
+    vault = gov.deploy(yVault, token, controller)
+    vault.setMin(10000)
+    return vault
+
+
+def test_deployment(strategy, vault, token, gov, controller):
+    # check strat values
+    assert strategy.governance() == gov
+    assert strategy.controller() == controller
+    assert strategy.getName() == STRAT_NAME
+
+    # check vault values
+    assert vault.governance() == gov
+    assert vault.controller() == controller
+    assert vault.token() == token
+    assert vault.name() == "yearn " + token.name()
+    assert vault.symbol() == "y" + token.symbol()
+    assert vault.decimals() == token.decimals()
+
+
+def max_approve(token, address, from_account):
+    token.approve(address, MAX_LIMIT, {"from": from_account})
---
+++ tests/functional/strategies/test_strategy_uni_cream.py
@@ -0,0 +1,50 @@
+import pytest
+import brownie
+
+from brownie import Wei, convert, accounts
+
+from brownie import StrategyCreamUNI, Controller, yVault
+
+MAX_LIMIT = 2 ** 256 - 1
+
+# UNI
+TOKEN_CONTRACT = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"
+
+STRAT_NAME = "StrategyCreamUNI"
+
+
+@pytest.fixture
+def token(Contract):
+    yield Contract.from_explorer(TOKEN_CONTRACT)
+
+
+@pytest.fixture
+def strategy(gov, controller):
+    strategy = gov.deploy(StrategyCreamUNI, controller)
+    return strategy
+
+
+@pytest.fixture
+def vault(gov, controller, strategy, token):
+    vault = gov.deploy(yVault, token, controller)
+    vault.setMin(10000)
+    return vault
+
+
+def test_deployment(strategy, vault, token, gov, controller):
+    # check strat values
+    assert strategy.governance() == gov
+    assert strategy.controller() == controller
+    assert strategy.getName() == STRAT_NAME
+
+    # check vault values
+    assert vault.governance() == gov
+    assert vault.controller() == controller
+    assert vault.token() == token
+    assert vault.name() == "yearn " + token.name()
+    assert vault.symbol() == "y" + token.symbol()
+    assert vault.decimals() == token.decimals()
+
+
+def max_approve(token, address, from_account):
+    token.approve(address, MAX_LIMIT, {"from": from_account})
