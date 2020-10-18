1c6e7c19d2ee223894a4ff7f90a3842d454b5194
Sascha Hanse <shanse@posteo.de>
2020-10-18 16:23:23 +0200

    test: add test for ETHOSMedianizer, #13 (#52)

--- tests/functional/utils/test_oracles.py
+++ tests/functional/utils/test_oracles.py
@@ -1,23 +1,45 @@
 import pytest
 
+from brownie import (
+    BTCOSMedianizer,
+    ETHOSMedianizer,
+)
 
-@pytest.fixture
-def oracle(a, BTCOSMedianizer):
-    yield a[0].deploy(BTCOSMedianizer)
-
-
-def test_hardcoded_config(a, oracle):
-    assert oracle.OSM() == "0xf185d0682d50819263941e5f4EacC763CC5C6C42"
-    assert oracle.MEDIANIZER() == "0x9B8Eb8b3d6e2e0Db36F41455185FEF7049a35CaE"
+ORACLES = [BTCOSMedianizer, ETHOSMedianizer]
 
 
-def test_governance(a, gov, oracle):
+@pytest.mark.parametrize(
+    "Oracle,osm,medianizer",
+    [
+        (
+            BTCOSMedianizer,
+            "0xf185d0682d50819263941e5f4EacC763CC5C6C42",
+            "0x9B8Eb8b3d6e2e0Db36F41455185FEF7049a35CaE",
+        ),
+        (
+            ETHOSMedianizer,
+            "0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763",
+            "0x729D19f657BD0614b4985Cf1D82531c67569197B",
+        ),
+    ],
+)
+def test_hardcoded_config(a, Oracle, osm, medianizer):
+    oracle = a[0].deploy(Oracle)
+    assert oracle.OSM() == osm
+    assert oracle.MEDIANIZER() == medianizer
+
+
+@pytest.mark.parametrize("Oracle", ORACLES)
+def test_governance(a, gov, Oracle):
+    oracle = a[0].deploy(Oracle)
     assert oracle.governance() == a[0]
     oracle.setGovernance(gov)
     assert oracle.governance() == gov
 
 
-def test_whitelist(gov, oracle):
+@pytest.mark.parametrize("Oracle", ORACLES)
+def test_whitelist(a, gov, Oracle):
+    oracle = a[0].deploy(Oracle)
     assert not oracle.authorized(gov)
     oracle.setAuthorized(gov)
     assert oracle.authorized(gov)
@@ -25,8 +47,10 @@ def test_whitelist(gov, oracle):
     assert not oracle.authorized(gov)
 
 
+@pytest.mark.parametrize("Oracle", ORACLES)
 @pytest.mark.parametrize("func", ["read", "foresight"])
-def test_read(a, oracle, func):
+def test_read(a, Oracle, func):
+    oracle = a[0].deploy(Oracle)
     price, osm = getattr(oracle, func)()
     assert price > 0
     assert not osm
