4dd2dfd0a5c40d54577c1c2c046c9b76f0e741d8
Sawit Trisirisatayawong <sawit@bandprotocol.com>
2020-10-29 22:48:58 +0700

    chore: add cream controller interface function comments

--- interfaces/cream/Controller.sol
+++ interfaces/cream/Controller.sol
@@ -3,5 +3,31 @@
 pragma solidity ^0.5.17;
 
 interface Creamtroller {
+    /**
+     * @notice Claim all the comp accrued by holder in all markets
+     * @param holder The address to claim COMP for
+     */
     function claimComp(address holder) external;
+
+    /**
+     * @notice Returns the assets an account has entered
+     * @param account The address of the account to pull assets for
+     * @return A dynamic list with the assets the account has entered
+     */
+    function getAssetsIn(address account) external view returns (CToken[] memory);
+
+    /**
+     * @notice Returns whether the given account is entered in the given asset
+     * @param account The address of the account to check
+     * @param cToken The cToken to check
+     * @return True if the account is in the asset, otherwise false.
+     */
+    function checkMembership(address account, CToken cToken) external view returns (bool);
+
+    /**
+     * @notice Add assets to be included in account liquidity calculation
+     * @param cTokens The list of addresses of the cToken markets to be enabled
+     * @return Success indicator for whether each corresponding market was entered
+     */
+    function enterMarkets(address[] memory cTokens) public returns (uint256[] memory);
 }
