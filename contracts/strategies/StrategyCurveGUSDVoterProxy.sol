3a65e5f0b9d277b90bef5acf0b58b8e36713d0a5
orbxball <orbxball@protonmail.com>
2020-12-07 16:45:02 -0500

    fix: balance readability and gas efficiency

--- contracts/strategies/StrategyCurveGUSDVoterProxy.sol
+++ contracts/strategies/StrategyCurveGUSDVoterProxy.sol
@@ -96,13 +96,13 @@ contract StrategyCurveGUSDVoterProxy {
         proxy = _proxy;
     }
 
-    function switchDex() external {
+    function switchDex(bool isUniswap) external {
         require(msg.sender == strategist || msg.sender == governance, "!authorized");
         // save gas and storage
-        if (dex == address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D)) {
-            dex = address(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);
-        } else {
+        if (isUniswap) {
             dex = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+        } else {
+            dex = address(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);
         }
     }
 
